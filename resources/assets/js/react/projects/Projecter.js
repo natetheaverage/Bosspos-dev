4f46b0c8b5797b3b9b97a59cc4958cba7890cf6e
natetheaverage <natetheaverage@gmail.com>
2015-06-07 13:08:40 -0600

    still working on projects.js

--- resources/assets/js/react/projects/Projecter.js
+++ resources/assets/js/react/projects/Projecter.js
@@ -36,15 +36,15 @@ var Projecter = React.createClass({
     },
     addConversation: function (message)
     {
-        var insert = this.state.projects[message[1].project_id].conversations[message[1].id] = message[1];
-        //console.log('THIS IS IN PROJECTER>> >> >> ', message[1].project_id);
+        var addToProjects = this.state.projects[message[1].project_id].conversations[message[1].id] = message[1];
+        var addToConversations = this.state.conversations[message[1].id] = message[1];
+        //console.log('THIS IS IN PROJECTER>> >> >> ', addToConversations);
         var projectArray = new ObjectToArray(this.state.projects);
         var projects = this.state.projects;
-        this.setState({ projectArray, projects });
-
         var conversations = this.state.conversations;
-        this.setState({ conversations, insert });
-        console.log('Projector.js -> addConversation id ',message[1].id, ' added to this project ', this.state.projects[message[1].project_id]);
+        this.setState({ projectArray, projects, conversations });
+
+        //console.log('Projector.js -> addConversation id ',message[1].id, ' added to this project ', this.state.projects[message[1].project_id]);
     },
     updateMessage: function (message)
     {
@@ -57,17 +57,15 @@ var Projecter = React.createClass({
     },
     addMessage: function (message)
     {
-        console.log('Projector.js -> addMessage  ', this.state.conversations);
-        console.log('Projector.js -> addMessage  ', message[1]);
+        //console.log('Projector.js -> addMessage  ', this.state.conversations);
+        //console.log('Projector.js -> addMessage  ', message[1]);
         var project_id = this.state.conversations[message[1].conversation_id].owner_id;
         var insert = this.state.projects[project_id].conversations[message[1].conversation_id].messages[message[1].id] = message[1];
         var projectArray = new ObjectToArray(this.state.projects);
         var projects = this.state.projects;
         this.setState({ projectArray, projects });
-       // console.log('Projector.js -> addMessage message[1].conversation_id ', message[1]);
     },
     render: function () {
-
         var newProjectTitle = (project) => <ProjectTitle project={project} title={project[1].title} />;
 
         return (
